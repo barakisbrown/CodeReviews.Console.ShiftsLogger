6e8057ef9b5bff701bd5992d657ae9f2a7343866
barakis <lokislayer@gmail.com>
2025-03-05 23:01:00 -0600

    The parent class to a one to many relationship

--- ShiftLogger.Barakisbrown/ShiftLogger.Barakisbrown/Models/Employee.cs
+++ ShiftLogger.Barakisbrown/ShiftLogger.Barakisbrown/Models/Employee.cs
@@ -1,13 +1,13 @@
-using System;
-
 namespace ShiftLogger.Barakisbrown.Models;
 
 public class Employee
 {
     public int Id { get; set; }
+
     public string FirstName {get;set;}
 
     public string LastName {get;set;}
 
-    public List<Shifts> Shifts  {get;set;}   
+    // Required reference navigation to principle
+    public ICollection<Shifts> shifts { get;set;} = new List<Shifts>();
 }
