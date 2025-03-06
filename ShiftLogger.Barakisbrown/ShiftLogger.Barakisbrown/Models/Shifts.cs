2ffd778fb894ba7ebe08ed2950c165d6a418b600
barakis <lokislayer@gmail.com>
2025-03-05 23:01:15 -0600

    The many poco for a 1 to many relationship

--- ShiftLogger.Barakisbrown/ShiftLogger.Barakisbrown/Models/Shifts.cs
+++ ShiftLogger.Barakisbrown/ShiftLogger.Barakisbrown/Models/Shifts.cs
@@ -5,7 +5,12 @@ public class Shifts
     public int Id {get;set;}
 
     public DateTime BeginShift {get;set;}
+
     public DateTime EndShift {get;set;}
 
-    public int EmployeeID {get;set;}
+    // FOREIGN KEY
+    public int EmployeeID { get; set; }
+
+    // Required Reference navigation to principal
+    public Employee Employee { get; set; } = null!;
 }
