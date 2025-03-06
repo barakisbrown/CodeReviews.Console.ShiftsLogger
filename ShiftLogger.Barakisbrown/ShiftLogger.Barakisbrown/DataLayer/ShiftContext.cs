5c8e2aa2439b3e16250fb9ce1354a6d586a28a82
barakis <lokislayer@gmail.com>
2025-03-05 23:01:41 -0600

    The Context to talk with the database

---
+++ ShiftLogger.Barakisbrown/ShiftLogger.Barakisbrown/DataLayer/ShiftContext.cs
@@ -0,0 +1,21 @@
+﻿namespace ShiftLogger.Barakisbrown.DataLayer;
+
+using Microsoft.EntityFrameworkCore;
+using Models;
+
+internal class ShiftContext : DbContext
+{
+    public DbSet<Employee> Employees { get; set; }
+    public DbSet<Shifts> Shifts { get; set; }
+    
+    public ShiftContext() { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Employee>()
+            .HasMany(e => e.shifts)
+            .WithOne(o => o.Employee)
+            .HasForeignKey(e => e.EmployeeID)
+            .IsRequired();
+    }
+}
