f27740aabdf7b33d9f2a0c85aec1db85ea174f8b
barakis <lokislayer@gmail.com>
2025-03-10 01:12:31 -0500

    Added DbContext
    Added DbContext to the application.  I forgot that I need to add the context to the program class since this was an api.

--- ShiftLogger.Barakisbrown/ShiftLogger.Barakisbrown/Program.cs
+++ ShiftLogger.Barakisbrown/ShiftLogger.Barakisbrown/Program.cs
@@ -1,11 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using ShiftLogger.Barakisbrown.DataLayer;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Add services to the container.
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+// ADD DB CONTEXT BELOW
+builder.Services.AddDbContext<ShiftContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
 
 var app = builder.Build();
 app.UseHttpsRedirection();
