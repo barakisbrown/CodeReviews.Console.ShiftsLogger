b524fd44e3a7adca9f8b4f2ba25a5bc802ef2c4e
barakis <lokislayer@gmail.com>
2025-03-12 15:47:41 -0500

    Added the interface and stubed implentation for service.

---
+++ ShiftLogger.Barakisbrown/ShiftLogger.Barakisbrown/Services/IShiftService.cs
@@ -0,0 +1,12 @@
+﻿using ShiftLogger.Barakisbrown.Models;
+
+namespace ShiftLogger.Barakisbrown.Services;
+
+public interface IShiftService
+{
+    Shifts CreateShift();
+    Shifts ViewShift(int EmployeeId);
+    Shifts EditShift(int EmployeeId);
+    bool DeleteShift(int ShiftId,int EmployeeID);
+    bool DeleteAllShifts(int EmployeeID);
+}
---
+++ ShiftLogger.Barakisbrown/ShiftLogger.Barakisbrown/Services/ShiftService.cs
@@ -0,0 +1,34 @@
+﻿using ShiftLogger.Barakisbrown.DataLayer;
+using ShiftLogger.Barakisbrown.Models;
+
+namespace ShiftLogger.Barakisbrown.Services;
+
+internal class ShiftService(ShiftContext context) : IShiftService
+{
+    private readonly ShiftContext _context = context;
+
+    public Shifts CreateShift()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool DeleteAllShifts(int EmployeeID)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool DeleteShift(int ShiftId, int EmployeeID)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Shifts EditShift(int EmployeeId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Shifts ViewShift(int EmployeeId)
+    {
+        throw new NotImplementedException();
+    }
+}
