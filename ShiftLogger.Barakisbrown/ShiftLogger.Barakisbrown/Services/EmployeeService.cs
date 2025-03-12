e12fd1683193a4ac344556b19adceb5f97dcd8c6
barakis <lokislayer@gmail.com>
2025-03-12 15:47:09 -0500

    Add Basic Service Inteface and base implementation.

---
+++ ShiftLogger.Barakisbrown/ShiftLogger.Barakisbrown/Services/EmployeeService.cs
@@ -0,0 +1,34 @@
+﻿using ShiftLogger.Barakisbrown.DataLayer;
+using ShiftLogger.Barakisbrown.Models;
+
+namespace ShiftLogger.Barakisbrown.Services;
+
+internal class EmployeeService(ShiftContext shiftContext) : IEmployeeService
+{
+    private readonly ShiftContext _shiftContext = shiftContext;
+
+    public bool AddEmployee()
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Employee> GetAllEmployees()
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Shifts> GetAllShifts(int Id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool RemoveEmployee()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool UpdateEmployee()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ ShiftLogger.Barakisbrown/ShiftLogger.Barakisbrown/Services/IEmployeeService.cs
@@ -0,0 +1,12 @@
+﻿using ShiftLogger.Barakisbrown.Models;
+
+namespace ShiftLogger.Barakisbrown.Services;
+
+public interface IEmployeeService
+{
+    public List<Shifts> GetAllShifts(int Id);
+    public List<Employee> GetAllEmployees();
+    public bool AddEmployee();
+    public bool RemoveEmployee();
+    public bool UpdateEmployee();
+}
