597989335cc14ed922837a66ba6204256ec57dd5
barakis <lokislayer@gmail.com>
2025-03-22 01:56:59 -0500

    Updated functions with correct parameters

--- ShiftLogger.Barakisbrown/ShiftLogger.Barakisbrown/Services/IEmployeeService.cs
+++ ShiftLogger.Barakisbrown/ShiftLogger.Barakisbrown/Services/IEmployeeService.cs
@@ -4,9 +4,9 @@ namespace ShiftLogger.Barakisbrown.Services;
 
 public interface IEmployeeService
 {
-    public List<Shifts> GetAllShifts(int Id);
+    public List<Shifts> GetAllShifts(int employeeId);
     public List<Employee> GetAllEmployees();
-    public bool AddEmployee();
-    public bool RemoveEmployee();
-    public bool UpdateEmployee();
+    public bool AddEmployee(string firstName,string lastName);
+    public bool RemoveEmployee(int removeId);
+    public bool UpdateEmployee(int updateId, string? firstName, string? lastName);
 }
