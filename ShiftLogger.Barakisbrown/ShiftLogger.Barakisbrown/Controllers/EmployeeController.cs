18c9a387dc1fce339f714470193dc94e8e79716a
barakis <lokislayer@gmail.com>
2025-03-24 22:56:35 -0500

    Started Working on Employee Api

---
+++ ShiftLogger.Barakisbrown/ShiftLogger.Barakisbrown/Controllers/EmployeeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShiftLogger.Barakisbrown.Models;
+using ShiftLogger.Barakisbrown.Services;
+
+namespace ShiftLogger.Barakisbrown.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IEmployeeService _employeeService;
+
+        public EmployeeController(IEmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Employee>> GetEmployees()
+        {
+            return await _employeeService.Get();
+        }
+
+        [HttpGet("{EmployeeId}")]
+        public async Task<ActionResult<Employee>> GetEmployee(int EmployeeId)
+        {
+            return await _employeeService.GetById(EmployeeId);
+        }
+    }
+}
