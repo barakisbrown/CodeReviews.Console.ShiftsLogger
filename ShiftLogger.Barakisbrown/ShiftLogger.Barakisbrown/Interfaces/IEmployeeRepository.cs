6dde26a675b77331d0c71918aa3290724bfcc41c
barakis <lokislayer@gmail.com>
2026-08-19 01:12:07 -0500

    Employee Repository Completed

---
+++ ShiftLogger.Barakisbrown/ShiftLogger.Barakisbrown/Interfaces/IEmployeeRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace ShiftLogger.Barakisbrown.Interfaces;
+
+using ShiftLogger.Barakisbrown.Models;
+
+public interface IEmployeeRepository
+{
+    Task<List<Employee>> GetAllAsync();
+
+    Task<Employee?> GetByIDAsync(int id);
+
+    Task<Employee> AddEmployeeAsync(Employee newEmployee);
+
+    Task<Employee ?> Update(Employee updatedEmployee);
+    Task<Employee?> Delete(int id);
+}
---
+++ ShiftLogger.Barakisbrown/ShiftLogger.Barakisbrown/Repository/EmployeeRespository.cs
@@ -0,0 +1,52 @@
+﻿namespace ShiftLogger.Barakisbrown.Repository;
+
+using Microsoft.EntityFrameworkCore;
+using ShiftLogger.Barakisbrown.DataLayer;
+using ShiftLogger.Barakisbrown.Interfaces;
+using ShiftLogger.Barakisbrown.Models;
+
+public class EmployeeRespository(ShiftContext context) : IEmployeeRepository
+{
+    private readonly ShiftContext _context = context;
+
+    public async Task<Employee> AddEmployeeAsync(Employee newEmployee)
+    {
+        await _context.Employees.AddAsync(newEmployee);
+        await _context.SaveChangesAsync();
+        return newEmployee;
+    }
+
+    public async Task<Employee?> Delete(int id)
+    {
+        var emp = await _context.Employees.FirstOrDefaultAsync(x => x.Id == id);
+        if (emp == null) return null;
+        _context.Remove(emp);
+        await _context.SaveChangesAsync();
+        return emp;
+
+    }
+
+    public async Task<List<Employee>> GetAllAsync()
+    {
+        return await _context.Employees.ToListAsync();
+    }
+
+    public async Task<Employee?> GetByIDAsync(int id)
+    {
+        return await _context.Employees.FindAsync(id);
+    }
+
+    public async Task<Employee?> Update(int id,Employee updatedEmployee)
+    {
+        var emp = await _context.Employees.FirstOrDefaultAsync(x => x.Id == id);
+
+        if (emp == null) return null;
+
+        emp.FirstName = updatedEmployee.FirstName;
+        emp.LastName = updatedEmployee.LastName;
+        emp.shifts = updatedEmployee.shifts;
+
+        await _context.SaveChangesAsync();
+        return emp;
+    }
+}
