a0720f5cf442afe3b10033c5ef6abe13968a36a7
barakis <lokislayer@gmail.com>
2026-08-19 00:47:04 -0500

    Restoring Service Class before branching and moving to a repository pattern

--- ShiftLogger.Barakisbrown/ShiftLogger.Barakisbrown/DTO/EmployeeResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ShiftLogger.Barakisbrown.DTO
-{
-    public class EmployeeResponse
-    {
-        public required string FirstName { get; set; } = string.Empty;
-
-        public required string LastName { get; set; } = string.Empty;
-    }
-}
