713b7816aa224b2bb8fa3331042c96e82cb31217
barakis <lokislayer@gmail.com>
2026-08-19 01:59:42 -0500

    Add Required Field

--- ShiftLogger.Barakisbrown/ShiftLogger.Barakisbrown/Models/Employee.cs
+++ ShiftLogger.Barakisbrown/ShiftLogger.Barakisbrown/Models/Employee.cs
@@ -4,9 +4,9 @@ public class Employee
 {
     public int Id { get; set; }
 
-    public string FirstName {get;set;}
+    public required string FirstName {get;set;}
 
-    public string LastName {get;set;}
+    public required string LastName {get;set;}
 
     // Required reference navigation to principle
     public ICollection<Shifts> shifts { get;set;} = new List<Shifts>();
