8ce6e56f911478a92c2484184187b5b68d16f0f4
barakis <lokislayer@gmail.com>
2026-08-20 17:47:29 -0500

    Employee DTO

---
+++ ShiftLogger.Barakisbrown/ShiftLogger.Barakisbrown/DTO/EmployeeDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace ShiftLogger.Barakisbrown.DTO
+{
+    public class EmployeeDTO
+    {
+        public required string FirstName { get; set; } = string.Empty;
+        public required string LastName { get; set; } = string.Empty;
+    }
+}
