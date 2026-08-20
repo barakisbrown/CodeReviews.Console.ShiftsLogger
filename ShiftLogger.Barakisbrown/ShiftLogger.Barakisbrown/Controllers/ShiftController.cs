162f7308208bd5abe6d5167b991bd4b9b8cd06c2
barakis <lokislayer@gmail.com>
2026-08-20 17:49:19 -0500

    Started working

---
+++ ShiftLogger.Barakisbrown/ShiftLogger.Barakisbrown/Controllers/ShiftController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShiftLogger.Barakisbrown.Interfaces;
+
+namespace ShiftLogger.Barakisbrown.Controllers
+{
+    [ApiController]
+    [Route("api/shift")]
+    public class ShiftController : ControllerBase
+    {
+        private readonly IShiftRepository _shiftRepo;
+
+        public ShiftController(IShiftRepository repo)
+        {
+            _shiftRepo = repo;
+        }
+
+        // Get an Employee Shifts
+        // Get: api/shift/{id}
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAllShifts(int id)
+        {
+            var shifts = await _shiftRepo.GetAllAsync(id);
+            if (shifts == null) return NotFound();
+            return Ok(shifts);
+
+        }
+    }
+}
