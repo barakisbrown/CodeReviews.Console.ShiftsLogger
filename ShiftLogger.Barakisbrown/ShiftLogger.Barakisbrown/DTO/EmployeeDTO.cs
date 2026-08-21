0de4d19a9e9b1f741d8e89f3e2b2cf61240ee0f9
barakis <lokislayer@gmail.com>
2026-08-20 19:35:06 -0500

    Added ID

--- ShiftLogger.Barakisbrown/ShiftLogger.Barakisbrown/DTO/EmployeeDTO.cs
+++ ShiftLogger.Barakisbrown/ShiftLogger.Barakisbrown/DTO/EmployeeDTO.cs
@@ -2,6 +2,7 @@
 {
     public class EmployeeDTO
     {
+        public int Id { get; set; }
         public required string FirstName { get; set; } = string.Empty;
         public required string LastName { get; set; } = string.Empty;
     }
