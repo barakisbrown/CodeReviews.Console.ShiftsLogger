41091fb7a2f17ad238a056602e327063a6043b48
barakis <lokislayer@gmail.com>
2026-08-20 19:34:45 -0500

    DTO based on Shift

---
+++ ShiftLogger.Barakisbrown/ShiftLogger.Barakisbrown/DTO/ShiftDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace ShiftLogger.Barakisbrown.DTO;
+
+public class ShiftDTO
+{
+    public int Id { get; set; }
+    public DateTime BeginShift { get; set; } = DateTime.Now;
+
+    public DateTime EndShift { get; set; } = DateTime.Now;
+}
