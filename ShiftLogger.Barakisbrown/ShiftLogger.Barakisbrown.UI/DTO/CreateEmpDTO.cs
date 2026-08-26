00a3bf78daa175925650d7958da9aae630331d17
barakis <lokislayer@gmail.com>
2026-08-26 00:27:21 -0500

    DTO for creating an employee

---
+++ ShiftLogger.Barakisbrown/ShiftLogger.Barakisbrown.UI/DTO/CreateEmpDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace ShiftLogger.Barakisbrown.UI.DTO;
+
+public class CreateEmpDTO
+{
+    public required string FirstName { get; set; } = string.Empty;
+    public required string LastName { get; set; } = string.Empty;    
+}
