7b9b6809cf2c8dc02de0c18c696330616861025b
barakis <lokislayer@gmail.com>
2026-08-26 00:27:35 -0500

    Still testing

--- ShiftLogger.Barakisbrown/ShiftLogger.Barakisbrown.UI/Program.cs
+++ ShiftLogger.Barakisbrown/ShiftLogger.Barakisbrown.UI/Program.cs
@@ -1,27 +1,29 @@
 ﻿
+using ShiftLogger.Barakisbrown.UI.DTO;
 using ShiftLogger.Barakisbrown.UI.Repos;
+using ShiftLogger.Barakisbrown.UI.UserInput;
 
-var repo = new EmployeeRepo();
+var empRepo = new EmployeeRepo();
+var shiftRepo = new ShiftsRepo();
 
-var emps = await repo.GetEmployeesAsync();
+var list = await empRepo.GetEmployeesAsync();
 
-Console.WriteLine($"Is emps null? {emps==null}");
-Console.WriteLine($"There are currently {emps.Count} employees");
-
-var index = 1;
-foreach(var emp in emps)
+Helper.ShowMsg($"There are currently {list.Count} employees");
+foreach (var emp in list)
 {
-    Console.WriteLine($"Employee {index}\tFirst Name = {emp.FirstName} \t Last Name = {emp.LastName}");
-    index++;
+    Helper.DisplayFullName(emp);
 }
 
-int firstID = 1, secondID = 4;
 
-var emp1 = await repo.GetEmployeeById(firstID);
-var emp2 = await repo.GetEmployeeById(secondID);
 
-Console.WriteLine($"Was the first one found? {(emp1 == null ? "Not Found" : "Found")}");
-Console.WriteLine($"Was the first one found? {(emp2 == null ? "Not Found" : "Found")}");
+
+
+
+
+
+
+
+
 
 
 Console.ReadKey(true);
