b695f3ca5fc60d3da635c419891ad48d39d6fed5
barakis <lokislayer@gmail.com>
2026-08-26 00:26:22 -0500

    For static methods that help the overall ui work better

---
+++ ShiftLogger.Barakisbrown/ShiftLogger.Barakisbrown.UI/UserInput/Helper.cs
@@ -0,0 +1,28 @@
+﻿using ShiftLogger.Barakisbrown.UI.Models;
+using Spectre.Console;
+
+namespace ShiftLogger.Barakisbrown.UI.UserInput;
+
+public static class Helper
+{
+    public static void ShowMsg(string msg)
+    {
+        AnsiConsole.MarkupLine(msg);
+    }
+
+    public static void ShowNotFound()
+    {
+        AnsiConsole.MarkupLineInterpolated($"[RED]Nothing Found Here![/]");
+    }
+
+    public static void ShowException(Exception xcpt)
+    {
+        AnsiConsole.WriteException(xcpt);
+    }
+
+    public static void DisplayFullName(Employee emp)
+    {
+        var fullName = emp.FirstName + " " + emp.LastName;
+        AnsiConsole.WriteLine(fullName);
+    }
+}
