bf42a7253e88493194f602c10bee0f39842bd8d7
barakis <lokislayer@gmail.com>
2026-08-26 00:26:48 -0500

    Added Shift Interface and Repo

---
+++ ShiftLogger.Barakisbrown/ShiftLogger.Barakisbrown.UI/Interfaces/IShiftRepo.cs
@@ -0,0 +1,8 @@
+﻿using ShiftLogger.Barakisbrown.UI.Models;
+
+namespace ShiftLogger.Barakisbrown.UI.Interfaces;
+
+public interface IShiftRepo
+{
+    Task<List<Shifts ?>> GetAllShiftsAsync();
+}
---
+++ ShiftLogger.Barakisbrown/ShiftLogger.Barakisbrown.UI/Repos/ShiftsRepo.cs
@@ -0,0 +1,41 @@
+﻿using ShiftLogger.Barakisbrown.UI.Interfaces;
+using ShiftLogger.Barakisbrown.UI.Models;
+using ShiftLogger.Barakisbrown.UI.UserInput;
+using Spectre.Console;
+using System.Net.Http.Json;
+
+namespace ShiftLogger.Barakisbrown.UI.Repos;
+
+public class ShiftsRepo : IShiftRepo
+{
+    private int portNumber = 5012;
+    private string baseUrl;
+
+    private HttpClient client;
+
+    public ShiftsRepo()
+    {
+        baseUrl = $"http://localhost:{portNumber}/api/shift/";
+    }
+
+    public async Task<List<Shifts ?>> GetAllShiftsAsync()
+    {
+        client = new()
+        {
+            BaseAddress = new Uri(baseUrl),
+        };
+
+        HttpResponseMessage message = await client.GetAsync(baseUrl);
+
+        if (message.StatusCode == System.Net.HttpStatusCode.NotFound)
+        {
+            Helper.ShowNotFound();
+            return null;
+        }
+
+       
+        List<Shifts?>? shifts = await client.GetFromJsonAsync<List<Shifts ?>>("");
+        if (shifts == null) return null;
+        return shifts;
+    }
+}
